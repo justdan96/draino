3f4a86ba7c8055b6fdccd546d7f4b579d0dd3d75
Daniel Henkel <daniel.henkel@datadoghq.com>
2022-11-01 16:41:10 +0100

    merge informer_test into informer/fake

--- internal/kubernetes/informer/fake.go
+++ internal/kubernetes/informer/fake.go
@@ -3,6 +3,9 @@ package informer
 import (
 	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+
 	corev1 "k8s.io/api/core/v1"
 	policyv1 "k8s.io/api/policy/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -50,4 +53,37 @@ func NewFakePDBInformer(ch chan struct{}, objects []runtime.Object) (PDBInformer
 
 func NewFakePodInformer(ch chan struct{}, objects []runtime.Object) (PodInformer, error) {
 	return NewFakeInformer(ch, objects)
+}
+
+func createPod(name, ns, nodeName string, isReady bool, ls ...labels.Set) *corev1.Pod {
+	var label labels.Set = map[string]string{}
+	if len(ls) > 0 {
+		label = ls[0]
+	}
+	ready := corev1.ConditionFalse
+	if isReady {
+		ready = corev1.ConditionTrue
+	}
+	return &corev1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "Pod",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: ns,
+			Labels:    label,
+		},
+		Spec: corev1.PodSpec{
+			NodeName: nodeName,
+		},
+		Status: corev1.PodStatus{
+			Conditions: []corev1.PodCondition{
+				{
+					Type:   corev1.ContainersReady,
+					Status: ready,
+				},
+			},
+		},
+	}
 }
