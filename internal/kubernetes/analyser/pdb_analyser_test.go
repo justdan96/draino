b53713aa0563591996d6d0d175f1eb4242ddda3a
David Benque <david.benque@datadoghq.com>
2023-01-06 00:30:55 +0100

    sorting node to have first node that take the PDB and that are likely to success drain

--- internal/kubernetes/analyser/pdb_analyser_test.go
+++ internal/kubernetes/analyser/pdb_analyser_test.go
@@ -156,7 +156,7 @@ func TestPDBAnalyser(t *testing.T) {
 			defer close(ch)
 			wrapper.Start(ch)
 
-			analyser := NewPDBAnalyser(indexer)
+			analyser := NewPDBAnalyser(context.Background(), zapr.NewLogger(zap.NewNop()), indexer)
 			pods, err := analyser.BlockingPodsOnNode(context.Background(), tt.NodeName)
 			assert.NoError(t, err)
 
@@ -202,7 +202,7 @@ func TestPDBAnalyser_IsPDBBlocked(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.Name, func(t *testing.T) {
-			res := IsPDBBlocked(context.Background(), tt.Pod, tt.PDB)
+			res := IsPDBBlockedByPod(context.Background(), tt.Pod, tt.PDB)
 			assert.Equal(t, tt.IsBlocked, res)
 		})
 	}
