8442984d9d31eeed7279215b0e901b7600cf214d
David Benque <david.benque@datadoghq.com>
2023-01-19 00:04:01 +0100

    drainBuffer diagnostics

--- internal/kubernetes/util_test.go
+++ internal/kubernetes/util_test.go
@@ -1,7 +1,18 @@
 package kubernetes
 
 import (
+	"context"
+	"fmt"
+	"github.com/go-logr/zapr"
+	"github.com/planetlabs/draino/internal/kubernetes/index"
+	"github.com/planetlabs/draino/internal/kubernetes/k8sclient"
+	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/apps/v1"
+	core "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	fakeclient "k8s.io/client-go/kubernetes/fake"
 	"reflect"
+	"sort"
 	"testing"
 
 	openapi_v2 "github.com/google/gnostic/openapiv2"
@@ -238,6 +249,340 @@ func TestGetAPIResourcesForGVK(t *testing.T) {
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetAPIResourcesForGVK() got = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestGetAnnotationFromNodeAndThenPodOrController(t *testing.T) {
+	testKey := "testKey"
+	testValue := "testValue"
+	nodeNoAnnotation := &core.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node1",
+		},
+	}
+	nodeNoKey := &core.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Annotations: map[string]string{},
+			Name:        "node1",
+		},
+	}
+	nodeWithKey := &core.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "node1",
+			Annotations: map[string]string{testKey: testValue},
+		},
+	}
+	podNoAnnotation := &core.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "no-annotation",
+			Namespace: "ns",
+			OwnerReferences: []metav1.OwnerReference{metav1.OwnerReference{
+				Controller: &isController,
+				Kind:       kindReplicaSet,
+				Name:       deploymentName + "-xyz",
+				APIVersion: "apps/v1",
+			}},
+		},
+		Spec: core.PodSpec{
+			NodeName: "node1",
+		},
+	}
+	podNoKey := &core.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "no-key",
+			Namespace:   "ns",
+			Annotations: map[string]string{},
+			OwnerReferences: []metav1.OwnerReference{metav1.OwnerReference{
+				Controller: &isController,
+				Kind:       kindReplicaSet,
+				Name:       deploymentName + "-xyz",
+				APIVersion: "apps/v1",
+			}},
+		},
+		Spec: core.PodSpec{
+			NodeName: "node1",
+		},
+	}
+	podWithKey := &core.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        "with-key",
+			Namespace:   "ns",
+			Annotations: map[string]string{testKey: testValue},
+			OwnerReferences: []metav1.OwnerReference{metav1.OwnerReference{
+				Controller: &isController,
+				Kind:       kindReplicaSet,
+				Name:       deploymentName + "-xyz",
+				APIVersion: "apps/v1",
+			}},
+		},
+		Spec: core.PodSpec{
+			NodeName: "node1",
+		},
+	}
+	DeploymentNoKey := &v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deploymentName,
+			Namespace: "ns",
+		},
+	}
+	DeploymentWithKey := &v1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        deploymentName,
+			Namespace:   "ns",
+			Annotations: map[string]string{testKey: testValue},
+		},
+	}
+
+	pods := map[string]*core.Pod{
+		podWithKey.Name:      podWithKey,
+		podNoKey.Name:        podNoKey,
+		podNoAnnotation.Name: podNoAnnotation,
+	}
+
+	tests := []struct {
+		name    string
+		node    *core.Node
+		objects []runtime.Object
+		want    AnnotationSearchResult[string]
+		wantErr bool
+	}{
+		{
+			name: "node no annotation",
+			node: nodeNoAnnotation,
+			want: AnnotationSearchResult[string]{
+				Found: false,
+			},
+		},
+		{
+			name: "node no key",
+			node: nodeNoKey,
+			want: AnnotationSearchResult[string]{
+				Found: false,
+			},
+		},
+		{
+			name: "node with key",
+			node: nodeWithKey,
+			want: AnnotationSearchResult[string]{
+				Value: testValue,
+				Found: true,
+			},
+		},
+		{
+			name: "node,pod no annotation, controller no key",
+			node: nodeNoAnnotation,
+			objects: []runtime.Object{
+				podNoAnnotation, DeploymentNoKey,
+			},
+			want: AnnotationSearchResult[string]{
+				Found: false,
+			},
+		},
+		{
+			name: "node,pod,controller no key",
+			node: nodeNoKey,
+			want: AnnotationSearchResult[string]{
+				Found: false,
+			},
+			objects: []runtime.Object{
+				podNoKey, DeploymentNoKey,
+			},
+		},
+		{
+			name: "node,pod,no key and controller with key",
+			node: nodeNoKey,
+			want: AnnotationSearchResult[string]{
+				Found: true,
+				PodResults: map[string][]AnnotationSearchResultOnPod[string]{
+					testValue: {{
+						Value:        testValue,
+						Pod:          podNoKey,
+						OnController: true,
+					}},
+				},
+			},
+			objects: []runtime.Object{
+				podNoKey, DeploymentWithKey, nodeNoKey,
+			},
+		},
+		{
+			name: "node no key, pod,controller with key",
+			node: nodeNoKey,
+			want: AnnotationSearchResult[string]{
+				Found: true,
+				PodResults: map[string][]AnnotationSearchResultOnPod[string]{
+					testValue: {{
+						Value:        testValue,
+						Pod:          podWithKey,
+						OnController: false,
+					}},
+				},
+			},
+			objects: []runtime.Object{
+				podWithKey, DeploymentWithKey,
+			},
+		},
+	}
+	testLogger := zapr.NewLogger(zap.NewNop())
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			wrapper, err := k8sclient.NewFakeClient(k8sclient.FakeConf{Objects: tt.objects})
+			assert.NoError(t, err)
+
+			fakeKubeClient := fakeclient.NewSimpleClientset(tt.objects...)
+			store, closeFunc := RunStoreForTest(context.Background(), fakeKubeClient)
+			defer closeFunc()
+
+			fakeIndexer, err := index.New(wrapper.GetManagerClient(), wrapper.GetCache(), testLogger)
+			assert.NoError(t, err)
+
+			ch := make(chan struct{})
+			defer close(ch)
+			wrapper.Start(ch)
+
+			if err != nil {
+				t.Fatalf("can't create fakeIndexer: %#v", err)
+			}
+
+			got, err := GetAnnotationFromNodeAndThenPodOrController(context.Background(), fakeIndexer, store, func(s string) (string, error) { return s, nil }, testKey, tt.node, true, true)
+			if tt.wantErr != (err != nil) {
+				fmt.Printf("%sGetAnnotationFromNodeAndThenPodOrController() ERR: %#v", tt.name, err)
+				t.Failed()
+			}
+			if err != nil {
+				return
+			}
+
+			// be sure that we are using the same pointer for comparison
+			for _, v := range got.PodResults {
+				for i := range v {
+					v[i].Pod = pods[v[i].Pod.Name]
+				}
+			}
+			for _, v := range tt.want.PodResults {
+				for i := range v {
+					v[i].Pod = pods[v[i].Pod.Name]
+				}
+			}
+
+			assert.Equalf(t, tt.want, got, "GetAnnotationFromNodeAndThenPodOrController()")
+		})
+	}
+}
+
+func TestAnnotationSearchResult_IsValueUnique(t *testing.T) {
+	tests := []struct {
+		name string
+		ar   AnnotationSearchResult[string]
+		want bool
+	}{
+		{
+			name: "empty",
+			ar:   AnnotationSearchResult[string]{},
+			want: false,
+		},
+		{
+			name: "node level only",
+			ar: AnnotationSearchResult[string]{
+				Found: true,
+				Value: "",
+				Node:  &core.Node{},
+			},
+			want: true,
+		},
+		{
+			name: "pod only and unique",
+			ar: AnnotationSearchResult[string]{
+				Found:      true,
+				PodResults: map[string][]AnnotationSearchResultOnPod[string]{"": {{Value: ""}}},
+			},
+			want: true,
+		},
+		{
+			name: "pod only not unique",
+			ar: AnnotationSearchResult[string]{
+				Found:      true,
+				PodResults: map[string][]AnnotationSearchResultOnPod[string]{"": {{Value: ""}}, "a": {{Value: "a"}}},
+			},
+			want: false,
+		},
+		{
+			name: "pod and node unique",
+			ar: AnnotationSearchResult[string]{
+				Found:      true,
+				Value:      "a",
+				Node:       &core.Node{},
+				PodResults: map[string][]AnnotationSearchResultOnPod[string]{"a": {{Value: "a"}}},
+			},
+			want: true,
+		},
+		{
+			name: "pod and node not unique",
+			ar: AnnotationSearchResult[string]{
+				Found:      true,
+				Value:      "a",
+				Node:       &core.Node{},
+				PodResults: map[string][]AnnotationSearchResultOnPod[string]{"b": {{Value: "b"}}},
+			},
+			want: false,
+		},
+		{
+			name: "pod and node not unique 2",
+			ar: AnnotationSearchResult[string]{
+				Found:      true,
+				Value:      "a",
+				Node:       &core.Node{},
+				PodResults: map[string][]AnnotationSearchResultOnPod[string]{"a": {{Value: "a"}}, "b": {{Value: "b"}}},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, tt.ar.IsValueUnique(), "IsValueUnique()")
+		})
+	}
+}
+
+func TestAnnotationSearchResult_AsSlice(t *testing.T) {
+	tests := []struct {
+		name    string
+		ar      AnnotationSearchResult[string]
+		wantOut []string
+	}{
+		{
+			name: "dupe node pod",
+			ar: AnnotationSearchResult[string]{
+				Found:      true,
+				Value:      "a",
+				Node:       &core.Node{},
+				PodResults: map[string][]AnnotationSearchResultOnPod[string]{"a": {{Value: "a"}}, "b": {{Value: "b"}}},
+			},
+			wantOut: []string{"a", "b"},
+		},
+		{
+			name: "pod only",
+			ar: AnnotationSearchResult[string]{
+				Found:      true,
+				PodResults: map[string][]AnnotationSearchResultOnPod[string]{"a": {{Value: "a"}}, "b": {{Value: "b"}}, "c": {{Value: "c"}}},
+			},
+			wantOut: []string{"a", "b", "c"},
+		},
+		{
+			name: "nil",
+			ar: AnnotationSearchResult[string]{
+				Found: false,
+			},
+			wantOut: nil,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sort.Strings(tt.wantOut)
+			sort.Strings(tt.ar.AsSlice())
+			assert.Equalf(t, tt.wantOut, tt.ar.AsSlice(), "AsSlice()")
 		})
 	}
 }
